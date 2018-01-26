9c183f77ea83916e9202be456eef6448b8b02093
xabiko <jdejprado@gmail.com>
2018-01-26 12:18:48 -0700

    target function generator

--- PLA_vs_SVM.cpp
+++ PLA_vs_SVM.cpp
@@ -17,6 +17,17 @@ struct binSet {
 	vector<bool>   y;
 };
 
+void target (float targ[]){
+	float ps[4];
+	for(int i=0; i<4; i++){
+		ps[i] = distribution(generator);
+	}
+	
+	targ[0] = (ps[3]-ps[1])/(ps[2]-ps[0]);
+	targ[1] = ps[1] - (targ[0]*ps[0]);
+
+}
+
 binSet gen (){
 	
 	binSet random_set;
@@ -32,10 +43,13 @@ binSet gen (){
 
 int main (){
 
+	float target_f[DIM];
+	target(target_f);
+
 	binSet test_set = gen();
 	binSet other_set= gen();
-	int i;
 
+	int i;
 	for(i=0; i<N; i++){
 		cout << test_set.x1[i] << " " << test_set.x2[i] << "\n";
 	}
@@ -43,6 +57,8 @@ int main (){
 	for(i=0; i<N; i++){
 		cout << other_set.x1[i] << " " << other_set.x2[i] << "\n";
 	}
-
+	cout << "\n";
+	cout << target_f[0]<< " " << target_f[1] << "\n";
+	
 	return 0;
 }
