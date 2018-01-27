ec524a5c6f6238884bacfa31a77cc0685428f646
xabiko <jdejprado@gmail.com>
2018-01-27 13:59:11 -0700

    output generation

--- PLA_vs_SVM.cpp
+++ PLA_vs_SVM.cpp
@@ -14,53 +14,49 @@ uniform_real_distribution<float> distribution(-1,1);
 struct binSet {
 	vector<float> x1;
 	vector<float> x2;
-	vector<bool>   y;
+	vector<int>   y ;
 };
 
-void target (float targ[]){
+void create_f (float arr[]){
 	float ps[4];
+
 	for(int i=0; i<4; i++){
 		ps[i] = distribution(generator);
 	}
-	
-	targ[0] = (ps[3]-ps[1])/(ps[2]-ps[0]);
-	targ[1] = ps[1] - (targ[0]*ps[0]);
-
+	arr[0] = (ps[3]-ps[1])/(ps[2]-ps[0]);
+	arr[1] = ps[1] - (arr[0]*ps[0]);
 }
 
-binSet gen (){
-	
+int bin_class(float target_f[], float &x1, float &x2){
+	float y = x1*target_f[0] - x2 + target_f[1];
+
+	if(y>0){return 1;}else
+	if(y<0){return -1;}
+	else{return 0;}
+}
+
+binSet gen (float target_f[]){
 	binSet random_set;
 
 	for(int i=0; i<N; i++){
                 random_set.x1.push_back(distribution(generator));
 		random_set.x2.push_back(distribution(generator));
+		random_set.y.push_back( bin_class(target_f, random_set.x1[i], random_set.x2[i]) );
         }
-	
 	return random_set;
-
 }
 
 int main (){
 
-	float target_f[DIM];
+	float hyperplane[DIM];
+	create_f(hyperplane);
 
-	binSet test_set = gen();
-	binSet other_set= gen();
+	binSet test_set = gen(hyperplane);
+	binSet other_set= gen(hyperplane);
 
-	int i;
-	for(i=0; i<N; i++){
-		cout << test_set.x1[i] << " " << test_set.x2[i] << "\n";
+	for(int i=0; i<N; i++){
+		cout << test_set.x1[i] << "\t" << test_set.x2[i] << "\t" << test_set.y[i] << "\n";
 	}
-	cout << "\n";
-	for(i=0; i<N; i++){
-		cout << other_set.x1[i] << " " << other_set.x2[i] << "\n";
-	}
-	cout << "\n";
-	for(i=0; i<N; i++){
-		target(target_f);
-		cout << target_f[0]<< " " << target_f[1] << "\n";
-	}	
 
 	return 0;
 }
